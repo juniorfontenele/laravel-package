c43f25cbcbd476638146c7d52ffc50a63c649075
Junior Fontenele <lcbfjr@gmail.com>
2024-07-16 09:56:18 -0300

    Add tests skeleton

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,16 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
-});
+namespace VendorName\Skeleton\Tests;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
